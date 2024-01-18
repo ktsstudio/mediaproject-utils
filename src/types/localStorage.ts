48fa57a7f273f68b43833f91c07deeff3db5c449
d.ragozin <dmtryragozin@gmail.com>
2024-01-18 18:13:47 +0300

    [*] Исправление потери типов typescript'ом

--- src/types/localStorage.ts
+++ src/types/localStorage.ts
@@ -2,4 +2,5 @@ export interface LocalStorage {
   setItem: (key: string, value: unknown) => void;
   getItem: (key: string) => unknown;
   removeItem: (key: string) => void;
+  [x: string]: unknown;
 }
